b519e120a02f26e3bbd37e3e859f1d3fafe51ab6
wiltondesigner <wiltondesigner@gmail.com>
2025-05-19 13:07:12 -0300

    Finalizei as rotas

---
+++ src/context/ProdutoContexto.jsx
@@ -0,0 +1,16 @@
+import { createContext} from 'react'
+
+const ProdutoContext = createContext()
+
+const contextProvedor = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export {ProdutoContext, contextProvedor} 
+
+
+
--- src/rotas/Rotas.jsx
+++ src/rotas/Rotas.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from '../components/Layout.jsx'
 import Home from '../pages/Home.jsx'
-import NotFound from '../pages/NotFound.jsx'
-import PageCarrinho from '../pages/PageCarrinho.jsx'
+
 
 const Rotas = () => {
   return (
@@ -11,8 +10,8 @@ const Rotas = () => {
       <Routes>
             <Route path='/' element={<Layout/>}>
                 <Route index element={<Home/>}/>
-                <Route path='carrinho' element={<ProductListingPage />}/>
-                <Route path='produtos/:id' element={<ProductViewPage />}/>   
+                <Route path='produtos' element={<ProductListingPage />}/>
+                <Route path='produto/:id' element={<ProductViewPage />}/>   
             </Route>
       </Routes>
       </BrowserRouter>
