33291a2c702a7164c668c9d47ba8f6f15af47d72
Luanny <luanny>
2025-05-20 08:56:40 -0300

    iniciando rotas

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Layout.css';
+import '../Layout.css';
 import foguinho from'../../assets/foguinho.png';
 import Ornament from'../../assets/Ornament 11.png';
 import WhiteSneakers from'../../assets/White-Sneakers-PNG-Clipart 1.png';
