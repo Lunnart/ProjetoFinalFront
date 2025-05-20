127d48e5d72ffcdd9045d9df4cceb4ea9c40bc6b
thaisbarreto43 <tatataisbarreto@gmail.com>
2025-05-20 16:28:37 -0300

    rascul

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
 
 const Footer = () => {
   return (
     <div>
-      fotter
-    </div>
+      <footer className=" bg-[#1f1f1f] w-1440px  h-454px  top-1873px  left-2526px"/>
+        <div class=" p-15">
+            <ul> <img src="footer/logo.png" alt="" /></ul>
+
+            <nav className=" flex text-justify">
+               <ul className=" flex-40 p-15">
+               <p className=" text-white">Lorem ipsum dolor sit amet, consectetur  adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore. </p>
+             </ul>
+
+
+
+            <ul className=" text-white flex-23 text-justify  ">
+               <li>
+                 <p className=" p-2 font-semibold">Informação</p>
+               </li>
+               <li><a className="p-2" href="">Sobre Drip Store</a></li>
+               <li><a className="p-2" href="">Segurança</a></li>
+               <li><a className="p-2" href="">Wishlist</a></li>
+               <li><a className="p-2" href="">Blog</a></li>
+               <li><a className="p-2" href="">Trabalhe Conosco</a></li>
+               <li><a className="p-2" href="">Meus Pedidos</a></li>
+
+            </ul>
+
+
+            <ul className=" text-white flex-23">
+             <li>
+               <p className="p-2 font-semibold ">Categorias</p>
+             </li>
+               <li><a className="p-2" href="">Camisetas</a></li>
+               <li><a className="p-2" href="">Calças</a></li>
+               <li><a className="p-2" href="">Bonés</a></li>
+               <li><a className="p-2" href="">Headphones</a></li>
+               <li><a className="p-2" href="">Ténis</a></li>
+            </ul>
+
+            <ul className=" text-white flex-23">
+              <li>
+                 <p className="p-2 font-semibold ">Contatos</p>
+              </li>
+                 <li className="p-2"> Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</li>
+                 <li className="p-2">(85) 3051-3411 </li>
+
+
+              </ul>
+              </nav>
+                 <div classNames=" flex " />
+                   <a className="m-5 cursor-pointer"><img src="footer/Path 22667 X.png" alt="twitter" /> </a>
+                   <a className="m-5 cursor-pointer "><img src="footer/Group 2555 (1).png" alt="instagram" /> </a>
+                   <a className="m-5 cursor-pointer"><img src="footer/Path 22671 FC.png" alt="facebook" /> </a>
+
+                  </div>
+
+          <div>
+            <div className=" text-white  justify-items-center p-2"/>
+               <img src="footer/Rectangle 3747.png" alt=" linha"/>
+               <p className=" text-white"> @2025 Digital College </p>
+            </div>
+      <footer/>
+    </div >
   )
 }
 
