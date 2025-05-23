4d5f476abd0ca15f9489c4a72ddf2da2dd361462
wiltondesigner <wiltondesigner@gmail.com>
2025-05-23 17:24:39 -0300

    refatorando header tailwind

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,11 +4,12 @@ import carrinho from "../../assets/carrinho.png"
 import logo from "../../assets/logo.png"
 import { FaSearch } from 'react-icons/fa'
 
+
 const Header = () => {
   return (
     <div>
         <header>
-    <div className="p1">
+    <div className="text-white flex justify-around my-[40px] mx-[30px]">
         <img src={logo} alt="" height="44px" width="253"/>
         <form className="form-header">
             <input type="text" id="text" name="Pesquisar" placeholder="Pesquisar produto..." className='text-light-gray h-12'/>
@@ -26,10 +27,10 @@ const Header = () => {
 
         <nav>
             <ul className="header">
-                <li><a href="#Home">Home</a></li>
-                <li><a href="#Produtos">Produtos</a></li>
-                <li><a href="#Categorias">Categorias</a></li>
-                <li><a href="#Meus Pedidos">Meus Pedidos</a></li>
+                <li><a className='hover:underline text-primary' href="#Home">Home</a></li>
+                <li><a className='hover:underline text-primary'href="#Produtos">Produtos</a></li>
+                <li><a className='hover:underline text-primary' href="#Categorias">Categorias</a></li>
+                <li><a className='hover:underline text-primary' href="#Meus Pedidos">Meus Pedidos</a></li>
             </ul>
         </nav>
 
