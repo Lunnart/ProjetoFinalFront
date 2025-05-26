83c56febe3d0abab60ce9d6b958d79acf25362b4
wiltondesigner <wiltondesigner@gmail.com>
2025-05-26 11:15:36 -0300

    parei no form-header input

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,30 @@ import { FaSearch } from 'react-icons/fa'
 // a:hover {
 //     color: #C92071;
 //     text-decoration: underline;
+//}
 
+
+
+//li>a {
+//    color: gray;
+//}
+
+//li:hover {
+    // color: gray;
+    // text-underline-offset: 5px;
+//}
+
+// .form-header {
+//     width: 40%;
+//     display: flex;
+// }
+
+// .form-header input {
+//     border: 1px solid #f2f2f2;
+//     background-color: #f2f2f2;
+//     border-radius: 5px 0 0 5px;
+//     width: 80%;
+// }
 
 const Header = () => {
   return (
@@ -15,13 +38,13 @@ const Header = () => {
         <header>
     <div className="text-white flex justify-around my-[40px] mx-[30px]">
         <img src={logo} alt="" height="44px" width="253"/>
-        <form className="form-header">
+        <form className="form-header flex w-40">
             <input type="text" id="text" name="Pesquisar" placeholder="Pesquisar produto..." className='text-light-gray h-12 p-3'/>
-            <button className="bg-white h-12 flex justify-end items-center">
+            <button className="bg-white h-12 flex justify-end items-center w-80">
               <FaSearch color='gray'/>
             </button>
         </form>
-        <label  className="text-light-gray font-inter" htmlFor="text"><a className='decoration-pink-500' href="#Cadastro">Cadastre-se</a></label>
+        <label  className="text-light-gray font-light" htmlFor="text"><a className='hover:underline font-bold decoration-2 underline-offset-[3px]' href="#Cadastro">Cadastre-se</a></label>
         <button className="bg-primary text-white font-bold rounded-lg p-1 text-center w-20"  type="button" >Entrar</button>
         
 
@@ -32,13 +55,12 @@ const Header = () => {
     </div>
 
     <div className="nav ml-15 p-[15 72 15 49] font-medium">
-
         <nav>
             <ul className="header ml-[10] p-[15 72 15 49] font-medium flex items-center gap-[36px] hover: bg-50 ">
-                <li><a className='hover:underline text-primary' href="#Home">Home</a></li>
-                <li><a className='hover:underline text-primary'href="#Produtos">Produtos</a></li>
-                <li><a className='hover:underline text-primary' href="#Categorias">Categorias</a></li>
-                <li><a className='hover:underline text-primary' href="#Meus Pedidos">Meus Pedidos</a></li>
+                <li><a className='hover:underline text-primary font-bold decoration-2 underline-offset-[3px]' href="#Home">Home</a></li>
+                <li><a className='hover:underline text-primary font-bold decoration-2 underline-offset-[3px]'href="#Produtos">Produtos</a></li>
+                <li><a className='hover:underline text-primary font-bold decoration-2 underline-offset-[3px]' href="#Categorias">Categorias</a></li>
+                <li><a className='hover:underline text-primary font-bold decoration-2 underline-offset-[3px]' href="#Meus Pedidos">Meus Pedidos</a></li>
             </ul>
         </nav>
 
