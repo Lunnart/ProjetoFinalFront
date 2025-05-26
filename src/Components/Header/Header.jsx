99ad0c436d66d1d4d756450b91a414879faa66c8
Luanny <luanny>
2025-05-25 22:05:46 -0300

    parei no button

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,9 @@ import carrinho from "../../assets/carrinho.png"
 import logo from "../../assets/logo.png"
 import { FaSearch } from 'react-icons/fa'
 
-
+// a:hover {
+//     color: #C92071;
+//     text-decoration: underline;
 
 
 const Header = () => {
@@ -19,16 +21,19 @@ const Header = () => {
               <FaSearch color='gray'/>
             </button>
         </form>
-        <label  className="text-light-gray" htmlFor="text"><a href="#Cadastro">Cadastre-se</a></label>
-        <button className=""  type="button" >Entrar</button>
-         {/* <a href="#" className="button">Entrar</a> */}
-        <img src={carrinho} alt="carrinho"/>
+        <label  className="text-light-gray font-inter" htmlFor="text"><a className='decoration-pink-500' href="#Cadastro">Cadastre-se</a></label>
+        <button className="bg-primary text-white font-bold"  type="button" >Entrar</button>
+
+        {/* <a href="#" className="text-light-gray font-inter">Favoritos</a> */}
+
+         
+        <img className='object-contain' src={carrinho} alt="carrinho"/>
     </div>
 
     <div className="nav ml-15 p-[15 72 15 49] font-medium">
 
         <nav>
-            <ul className="header ml-[10] p-[15 72 15 49] font-medium flex items-center gap-[36px] hover:text-pink-500">
+            <ul className="header ml-[10] p-[15 72 15 49] font-medium flex items-center gap-[36px] hover: bg-50">
                 <li><a className='hover:underline text-primary' href="#Home">Home</a></li>
                 <li><a className='hover:underline text-primary'href="#Produtos">Produtos</a></li>
                 <li><a className='hover:underline text-primary' href="#Categorias">Categorias</a></li>
