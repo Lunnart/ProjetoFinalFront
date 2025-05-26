7a202a37fbbf293f01f0ba8df4b28c3a56468920
Luanny <luanny>
2025-05-26 09:03:55 -0300

    cont header p tailwind

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -16,13 +16,14 @@ const Header = () => {
     <div className="text-white flex justify-around my-[40px] mx-[30px]">
         <img src={logo} alt="" height="44px" width="253"/>
         <form className="form-header">
-            <input type="text" id="text" name="Pesquisar" placeholder="Pesquisar produto..." className='text-light-gray h-12'/>
+            <input type="text" id="text" name="Pesquisar" placeholder="Pesquisar produto..." className='text-light-gray h-12 p-3'/>
             <button className="bg-white h-12 flex justify-end items-center">
               <FaSearch color='gray'/>
             </button>
         </form>
         <label  className="text-light-gray font-inter" htmlFor="text"><a className='decoration-pink-500' href="#Cadastro">Cadastre-se</a></label>
-        <button className="bg-primary text-white font-bold"  type="button" >Entrar</button>
+        <button className="bg-primary text-white font-bold rounded-lg p-1 text-center w-20"  type="button" >Entrar</button>
+        
 
         {/* <a href="#" className="text-light-gray font-inter">Favoritos</a> */}
 
@@ -33,7 +34,7 @@ const Header = () => {
     <div className="nav ml-15 p-[15 72 15 49] font-medium">
 
         <nav>
-            <ul className="header ml-[10] p-[15 72 15 49] font-medium flex items-center gap-[36px] hover: bg-50">
+            <ul className="header ml-[10] p-[15 72 15 49] font-medium flex items-center gap-[36px] hover: bg-50 ">
                 <li><a className='hover:underline text-primary' href="#Home">Home</a></li>
                 <li><a className='hover:underline text-primary'href="#Produtos">Produtos</a></li>
                 <li><a className='hover:underline text-primary' href="#Categorias">Categorias</a></li>
