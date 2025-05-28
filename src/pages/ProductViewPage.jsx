ef70c814991dfbe0b848a9be1d433a21cf1c64c3
thaisbarreto43 <tatataisbarreto@gmail.com>
2025-05-28 17:29:01 -0300

    nao adicionar a main

--- src/pages/ProductViewPage.jsx
+++ src/pages/ProductViewPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import BuyBox from '../Components/Buybox/'
 
 const ProductViewPage = () => {
   return (
     <div>
       <h1>Página de Visualização de 1 produto</h1>
+    
+    <BuyBox/>
+    
     </div>
+    
   )
 }
 
