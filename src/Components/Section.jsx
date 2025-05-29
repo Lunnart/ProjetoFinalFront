f975b8cc6f2d72a07b2dcc80804645eb16f288e7
wiltondesigner <wiltondesigner@gmail.com>
2025-05-29 16:37:35 -0300

    coleçao card finalizado

--- src/Components/Section.jsx
+++ src/Components/Section.jsx
@@ -4,7 +4,7 @@ import seta from '../assets/seta.png'
 
 const Section = ({title="Coleções em destaque", textLeft, children, link}) => {
   return (
-    <section className='px-[100px] bg-[#F9F8FE] pt-[38px]'>
+    <section className='p-[5%] bg-[#F9F8FE] pt-[38px]'>
          <div className='flex justify-between items-center '>
          <h1 className={`${textLeft? 'text-left': 'text-center'} font-bold text-2xl text-dark-gray-2 mb-5`}>{title}</h1>
         {link && <Link className='text-primary'>Ver todos <img className='inline' src={seta} alt="seta link"/></Link>}
