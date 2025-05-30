9b16b7a3b80959331936548f1e02c4ae89d32fa1
thaisbarreto43 <tatataisbarreto@gmail.com>
2025-05-30 12:20:16 -0300

    atualizacao

--- src/Components/Buybox.jsx
+++ src/Components/Buybox.jsx
@@ -4,40 +4,37 @@ import tenis from '../assets/tenis-carrossel.png'
 
 const Buybox = () => {
     return (
-
-
-        < >
-
+        <>
             <div className='bg-gray-50 pb-10 pt-5 pl-5 '>
                 <p className=" font-semibold  text-[#474747] "> Home | Produtos | Tênis | Nike | Tênis Nike Revolution 6 Next
                     Masculino </p>
             </div>
-            <div className='flex  bg-gray-50 pl-5'>
+            <div className='flex gap-[40px] bg-gray-50 px-[100px]'>
 
-                <div className='w-[50%] '>
+                <div className='w-[50%]'>
 
                     <div className=" bg-[#E2E3FF] duration-700 p-20 ease-in-out ">
                         <img src={tenis} className="object-cover" alt="Slide 2" />
                     </div>
-                    <div className=' flex'>
-                        <div className='bg-[#E2E3FF] drop-shadow-xl cursor-pointer rounded-sm w-[117.81px] m-2'  >
+                    <div className=' flex gap-2 w-full'>
+                        <div className='bg-[#E2E3FF] drop-shadow-xl cursor-pointer rounded-sm w-[117.81px] my-2'  >
                             <button><img src={tenis} alt="" className='cursor-pointer p-3 ' /></button>
                         </div>
 
-                        <div className='bg-[#FFE8BC] drop-shadow-xl cursor-pointer  rounded-sm w-[117.81px] m-2'  >
+                        <div className='bg-[#FFE8BC] drop-shadow-xl cursor-pointer  rounded-sm w-[117.81px] my-2'  >
 
                             <button><img src={tenis} alt="" className='cursor-pointer p-3 ' /></button>
                         </div>
 
-                        <div className='bg-[#FFC0BC] drop-shadow-xl cursor-pointer rounded-sm w-[117.81px] m-2'  >
+                        <div className='bg-[#FFC0BC] drop-shadow-xl cursor-pointer rounded-sm w-[117.81px] my-2'  >
                             <button><img src={tenis} alt="" className='cursor-pointer p-3 ' /></button>
                         </div>
 
-                        <div className='bg-[#DEC699] drop-shadow-xl cursor-pointer  rounded-sm w-[117.81px] m-2'  >
+                        <div className='bg-[#DEC699] drop-shadow-xl cursor-pointer  rounded-sm w-[117.81px] my-2'  >
                             <button><img src={tenis} alt="" className='cursor-pointer p-3 ' /></button>
                         </div>
 
-                        <div className='bg-[#E8DFCF] drop-shadow-xl cursor-pointer rounded-sm w-[117.81px] m-2'  >
+                        <div className='bg-[#E8DFCF] drop-shadow-xl cursor-pointer rounded-sm w-[117.81px] my-2'  >
                             <button><img src={tenis} alt="" className='cursor-pointer p-3 ' /></button>
                         </div>
 
@@ -47,7 +44,7 @@ const Buybox = () => {
 
 
 
-                <div className=' w-[50%] pl-6 '>
+                <div className='w-[50%]'>
                     <h1 className="font-bold text-[#1f1f1f]  text-[32px] pl-2 pr-60"> Tênis Nike Revolution   6 Next Nature Masculino
                     </h1>
                     <h2 className="text-[#666666] text-[12px] font-semibold pl-2"> Casual | Nike | REF:38416711 </h2>
@@ -64,7 +61,7 @@ const Buybox = () => {
                         <p className=" text-[15px] pt-5 text-[#cccccc] line-through ">250,00</p>
                     </div>
                     <h3 className="text-[#8f8f8f] font-semibold text-[15px] pl-2"> Descrição Do Produto </h3>
-                    <p className=" text-[#474747] font-semibold pl-2 pr-[208px] pb-[20px] "> Lorem ipsum dolor sit amet consectetur adipisicing elit.Odio
+                    <p className=" text-[#474747] font-semibold pl-2 pr-[80px] pb-[20px] "> Lorem ipsum dolor sit amet consectetur adipisicing elit.Odio
                         deleniti, fugit enim sit cum ea accusamus
                         molestias voluptas qui reprehenderit, delectus doloremque dolorum ab dolore sint? Voluptatum corrupti minus
                         quas.</p>
