045975c37f27fb04e84f2e3c77d17c7377b96489
Lunnart <luannygadelha@gmail.com>
2025-06-07 12:09:19 -0300

    removendo função

--- src/Components/Carrosel.jsx
+++ src/Components/Carrosel.jsx
@@ -4,6 +4,8 @@ import circle from '../assets/Ornament 11.png';
 import tenis from '../assets/White-Sneakers-PNG-Clipart 1.png';
 
 const Carrosel = () => {
+  
+    
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <div className="section-container flex">
